1fa0ff950272a867099362c752b1d185d41b3ecb
Moses Ogwo <mosesogwo@gmail.com>
2025-01-17 16:53:52 +0100

    update views

--- app/javascript/timestamp_formatter.js
+++ app/javascript/timestamp_formatter.js
@@ -1,21 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const formatTimestamps = () => {
     document.querySelectorAll(".timestamp").forEach((element) => {
-    const utcTimestamp = element.getAttribute("data-utc");
-    if (utcTimestamp) {
+      const utcTimestamp = element.getAttribute("data-utc");
+      if (utcTimestamp) {
         const localDate = new Date(utcTimestamp);
         const formattedDate = localDate.toLocaleString("en-GB", {
-          day: "2-digit",
           month: "short",
-          year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: true,
+          day: "2-digit",
+          year: "numeric"
+          // hour: "2-digit",
+          // minute: "2-digit",
+          // hour12: true,
         });
         element.textContent = formattedDate;
-    }
+      }
     });
   };
 
+  // Format timestamps on initial page load
   formatTimestamps();
+
+  // Re-run the function if Turbo (or other frameworks) replaces content
+  document.addEventListener('turbo:load', formatTimestamps);
 });
